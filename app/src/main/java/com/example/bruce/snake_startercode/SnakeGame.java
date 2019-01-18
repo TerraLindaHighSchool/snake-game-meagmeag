949ea281cfba0470090931da74561c9837f7ab80
mb00126 <meaghanb101@gmail.com>
2019-01-18 14:24:46 -0800

    Lab 2 WIP

--- app/src/main/java/com/example/bruce/snake_startercode/SnakeGame.java
+++ app/src/main/java/com/example/bruce/snake_startercode/SnakeGame.java
@@ -1,9 +1,29 @@
 package com.example.bruce.snake_startercode;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class SnakeGame {
+  private int mSpriteDim, mBOARD_WIDTH, mBOARD_HEIGHT, mScore,
+          mLevel, mCountdown, mMillisDelay;
+  int[] mAppleCoord;
+  ArrayList<SnakeSegment.BodyParts> mSnake = new ArrayList();
+  boolean mGameOver;
+
   
   public SnakeGame(int beginningDirection, int beginningSpriteDim, int beginningX, int beginningY, int width, int height){
-  
+    mSpriteDim = beginningSpriteDim;
+    mBOARD_WIDTH = width;
+    mBOARD_HEIGHT = height;
+    mScore = 0;
+    mLevel = 1;
+    mCountdown = 12;
+    mMillisDelay = 400;
+    mAppleCoord = new int[2];
+    mSnake.add(SnakeSegment.BodyParts.HEAD);
+    mSnake.add(SnakeSegment.BodyParts.BODY);
+    mSnake.add(SnakeSegment.BodyParts.TAIL);
+    mGameOver = false;
   }
   
   protected void touched(float xTouched, float yTouched){
@@ -16,5 +36,39 @@ public class SnakeGame {
     
   protected boolean play(){
         return false;
+  }
+
+  //Getters and Setters
+
+  public int getSpriteDim(){
+    return mSpriteDim;
+  }
+
+  public int getMillisDelay(){
+    return mMillisDelay;
+  }
+
+  public ArrayList getSnake(){
+    return mSnake;
+  }
+
+  public int[] getAppleCoord(){
+    return mAppleCoord;
+  }
+
+  public int getScore(){
+    return mScore;
+  }
+
+  public int getLevel(){
+    return mLevel;
+  }
+
+  public int getCountdown(){
+    return mCountdown;
+  }
+
+  public boolean getGameOver(){
+    return mGameOver;
   }
 }
